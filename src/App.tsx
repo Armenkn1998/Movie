6384d3bfea56f7c97c17301aefb312411e647d1b
Armenkn1998 <armen.kn1998@mail.ru>
2025-07-25 02:01:53 +0400

    movie

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import MenuBar from "./component/MenuBar";
+import HomePage from "./component/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main_container">
+      <MenuBar />
+      <HomePage />
+      </div>
+  
+    </div>
+  );
+}
+
+export default App;
